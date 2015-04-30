04c9fb58507e7a3f0ea97375191678ac580ce0eb
retuw <Seichan@Pearces-MacBook-Pro.local>
2015-04-29 19:17:56 -0700

    Added to Tile.h Included RobotAI files

--- maze.cpp
+++ maze.cpp
@@ -8,15 +8,15 @@ using namespace std;
 Maze::Maze()
 // ##############################################################
 {
-	for(int i = 0; i < 80; i++)	// loops trough the rows of the maze
+	for(int i = 0; i < 40; i++)	// loops trough the rows of the maze
 	{
-		for(int j = 0; j < 40; j++)	// loops through the columns of the maze
+		for(int j = 0; j < 80; j++)	// loops through the columns of the maze
 		{
 			Board[i][j].setWall();	// sets all tiles to wall
 		}
-		
+
 	}
-	
+
 }
 // ##############################################################
 
@@ -24,15 +24,15 @@ Maze::Maze()
 void Maze::display()
 // ##############################################################
 {
-	
-	for(int i = 0; i < 80; i++)	// loops through the rows of the maze
+
+	for(int i = 0; i < 40; i++)	// loops through the rows of the maze
 	{
-		for(int j = 0; j < 40; j++) // loops through the columns of the maze
+		for(int j = 0; j < 80; j++) // loops through the columns of the maze
 			cout << Board[i][j].getTile(); // gets a tile
-		cout << endl;	
+		cout << endl;
 	}
 }
 // ##############################################################
 
 
-		
+
--- tile.h
+++ tile.h
@@ -9,21 +9,33 @@ class Tile 	// creates an object Tile
 {
 	private:
 		char tile; // character tile
-	
+        bool isStart,
+             isEnd,
+             isWall;
+        char originDirection;
+
 	public:
 		Tile();	// constructor to initialize a tile
 
 		char setWall(); // will set the tile to a wall
 		char setPath();	// will set a tile to a path
+		void setIsStart(bool isStart);
+        void setIsEnd(bool isEnd);
+        void setIsWall(char tile);
+        void setOriginDirection(bool originDirection);
 
 		char getTile() const {return tile;}	// returns the value of the tile
+        bool getIsStart()const {return isStart;}
+        bool getIsEnd()const {return isEnd;}
+        bool getIsWall()const {return isWall;}
+        bool getOriginDirection()const {return originDirection;}
 
 
-		bool isWall(char tile);	// true if Tile is wall
-		bool isPath(char tile);	// true if Tile is a path
+		//bool isWall(char tile);	// true if Tile is wall
+		//bool isPath(char tile);	// true if Tile is a path <- *****this function seems to be redudant, can you explain its need?*****
 
-		
-	
+
+
 };
 
 #endif
