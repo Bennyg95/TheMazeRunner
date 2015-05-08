6f2789b2cfd3efd5f5151d1304c72ff0ef09730d
Daniel Perez <danielperez.csit@gmail.com>
2015-05-08 13:32:47 -0700

    created basic driver file

---
+++ mazeDriver.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    cout << "Welcome! Say Hello to the Maze Runner." << endl;
+    cout << "The Maze Runner is uses AI to navigate through a selected maze." << endl;
+    cout << "Lets begin." << endl << endl;
+
+
+
+
+    system("PAUSE");
+    return 0;
+}
