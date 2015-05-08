dbfeb53148ebf1acdab002f818aeb7f15fc45ab8
Daniel Perez <danielperez.csit@gmail.com>
2015-05-08 14:32:56 -0700

    driver update

--- mazeDriver.cpp
+++ mazeDriver.cpp
@@ -5,14 +5,23 @@ using namespace std;
 
 int main()
 {
+    char menuSelection;
+
     cout << "Welcome! Say Hello to the Maze Runner." << endl;
-    cout << "The Maze Runner is uses AI to navigate through a selected maze." << endl;
-    cout << "Lets begin." << endl << endl;
+    cout << "The Maze Runner is a robot that uses AI to navigate through a selected maze." << endl;
+    cout << "Press any Key to begin." << endl << endl;
 
+    //system("PAUSE");
+    cin.ignore(); // ignores the newline
+    cin.get(); //waits for character
 
+    //system("cls");
+    for(int i = 0; i < 25; i++)
+    {
+        cout << i <<endl;
+    }
 
-
-    system("PAUSE");
-    system("cls");
+    cin.ignore(); // ignores the newline
+    cin.get(); //waits for character
     return 0;
 }
