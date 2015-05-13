dcc9c41646c066cacbeddf0470b13a187eba48f2
Bennyg95 <sanchez.benitog95@hotmail.com>
2015-05-13 09:36:57 -0700

    Added a boarder around the maze.

--- maze.cpp
+++ maze.cpp
@@ -72,12 +72,25 @@ Maze::Maze()
 void Maze::display()
 // ##############################################################
 {
+	for(int i = 0; i < 52; i++)
+	{
+		cout << "#";
+	}
+	cout << endl;
+
 	for(int i = 0; i < 50; i++)	// loops through the rows of the maze
 	{
+		cout << "#";
 		for(int j = 0; j < 40; j++) // loops through the columns of the maze
 			cout << board[i][j].getTile(); // gets a tile
-		cout << endl;
+		cout << "#" << endl;
 	}
+	
+	for(int i = 0; i < 52; i++)
+	{
+		cout << "#";
+	}
+	cout << endl;
 }
 // ##############################################################
 
